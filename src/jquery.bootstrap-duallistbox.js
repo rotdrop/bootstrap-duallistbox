90a7dd13ece1b534e35934c7276f31432efae5d7
Claus-Justus Heine <himself@claus-justus-heine.de>
2026-09-03 00:30:32 +0200

    Copy with jq deprecations, avoid window.jQuery

--- src/jquery.bootstrap-duallistbox.js
+++ src/jquery.bootstrap-duallistbox.js
@@ -2,22 +2,12 @@
   if (typeof define === 'function' && define.amd) {
     define(['jquery'], factory);
   } else if (typeof module === 'object' && module.exports) {
-    module.exports = function(root, jQuery) {
-      if (jQuery === undefined) {
-        if (typeof window !== 'undefined') {
-          jQuery = require('jquery');
-        }
-        else {
-          jQuery = require('jquery')(root);
-        }
-      }
-      factory(jQuery);
-      return jQuery;
-    };
+    module.exports = factory(require('jquery'));
   } else {
-    factory(jQuery);
+    factory(require('jquery'));
   }
 }(function($) {
+
   // Create the defaults once
   var pluginName = 'bootstrapDualListbox',
     defaults = {
